56c4ffdc3eaa1a36d3a1645be7d827507dd96bb2
Msyoka <daviemumo37@gmail.com>
2019-05-31 14:44:11 +0300

    add scriptsfile and linking it to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-var sideA =parseInt(prompt("Enter lenght of side AB"));
-var sideB = parseInt(prompt("Enter length of BC"));
-var sideC =parseInt(prompt("Enter length of AC"));
-if(sideA ==sideB && sideB==sideC){
-alert("Equilateral");
-}else if((sideA==sideB && sideB!=sideC ) || (sideA!=sideB && sideC==sideA) || (sideC==sideB && sideC!=sideA)){
-alert("isosceles");
-}else if(sideA!=sideB && sideB!=sideC && sideC!=sideA){
-alert("scalene");
-}else if(sideA >= (sideB+sideC) || sideC >= (sideB+sideA) || sideB >= (sideA+sideC)){
-alert("No triangle");
+function tracker() {
+  var sideA = parseInt(document.getElementById('side1'));
+  var sideB = parseInt(document.getElementById('side2'));
+  var sideC = parseInt(document.getElementById('side3'));
+  if(sideA ===sideB && sideB===sideC){
+  alert("Equilateral");
+}else if((sideA===sideB && sideB!==sideC ) || (sideA!==sideB && sideC===sideA) || (sideC===sideB && sideC!=sideA)){
+  alert("isosceles");
+}else if(sideA!==sideB && sideB!==sideC && sideC!==sideA){
+  alert("scalene");
+  }else if(sideA >= (sideB+sideC) || sideC >= (sideB+sideA) || sideB >= (sideA+sideC)){
+  alert("No triangle");
+  }
 }
