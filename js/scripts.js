a0a7f154b2fa084e64cb6c9a4266c56a29048d2b
Msyoka <daviemumo37@gmail.com>
2019-06-02 12:41:05 +0300

    add changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function tracker() {
-  var sideA = parseInt(document.getElementById('side1'));
-  var sideB = parseInt(document.getElementById('side2'));
-  var sideC = parseInt(document.getElementById('side3'));
-  if(sideA + sideB >sideC &&sideA + sideC >sideB &&sideC + sideB >sideA){
+  var sideA = parseInt(document.getElementById('side1').value);
+  var sideB = parseInt(document.getElementById('side2').value);
+  var sideC = parseInt(document.getElementById('side3').value);
+
   if(sideA===sideB && sideB===sideC){
   alert("equilateral");
   }else if(sideA===sideB || sideA===sideC || sideB===sideC){
   alert("isosceles");
   }else if(sideA!=sideB && sideB!=sideC && sideA!=sideC){
   alert("scalene");
-  }else{
-  alert("Not a triangle")
- }
+  }else if (if(sideA + sideB >sideC &&sideA + sideC >sideB &&sideC + sideB >sideA) {
+  alert("Not a triangle");
+}
 }
